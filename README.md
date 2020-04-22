12cc9140bd57c513c7fb775b5c0f08cc8cfcc7b8
Hampus Ram <hampus.ram@educ.goteborg.se>
2020-04-22 10:29:20 +0200

    Initial commit of template code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # parcel-vue-template
-Very simple start template for Vue with Parcel.
+
+Very simple start template for Vue with Parcel. This code is intended for use with assignments for courses at Yrgo.
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <img src="../assets/images/rainbow.png" alt="A rainbow" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped lang="scss"></style>
